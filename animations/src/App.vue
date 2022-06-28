6385d1c766c57238cefc089a3efe7e5b610dbb5f
Michael <michaelmin2020@gmail.com>
2022-06-28 16:34:21 -0500

    added styling

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,9 +1,15 @@
 <template>
 <button type="button" @click="flag = !flag">Toggle</button>
-<transition name="fade" mode="out-in">
+
+<!-- <transition name="fade" mode="out-in">
 <h2 v-if="flag" key="main">Hello world!</h2>
 <h2 v-else key="secondary">Another hello!</h2>
+</transition> -->
+
+<transition name="zoom">
+<h2 v-if="flag">Hello</h2>
 </transition>
+
 </template>
 
 <script>
@@ -18,16 +24,22 @@ export default {
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 2s linear;
+  transition: all .75s linear;
 }
 
 .fade-leave-to {
-  transition: all 2s linear;
+  transition: all .75s linear;
   opacity: 0;
 }
 </style>
